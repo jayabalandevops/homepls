acc0273a48b9a7dfa730143e8ead247fb692a045
jayabalandevops <balaniaf@gmail.com>
2019-10-19 08:21:01 +0530

    stage build

--- app/build.groovy
+++ app/build.groovy
@@ -1,4 +1,4 @@
-import groovy.json.SlurperClassic
+import groovy.json.JsonSlurperClassic
 cDelivery = readFile "$wrks/config/pls.json"
 def jString = new JsonSlurperClassic().parseText(cDelivery)
 def bStr = jString.btool
