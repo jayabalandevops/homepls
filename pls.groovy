4e6f3cd39386f6c6733f9bd8bc5a5ad0487b642a
jayabalandevops <balaniaf@gmail.com>
2019-10-19 06:01:49 +0530

    added pls.groovy and touched cit.groovy

---
+++ pls.groovy
@@ -0,0 +1,28 @@
+folder("$BUName"){}
+folder("$ProductName")
+pipelineJob("$BUName/$ProductName/CICD_$AppName"){
+    parameters{
+        stringParam("AppRepo", "$ApplicationRepo","Git URL")
+        stringParam("AppName", "$AppName", "App Name")
+        stringParam("UnitTestTool", "$UnitTestRun","")
+        stringParam("NodeName", "$NodeName", "")
+        activeChoiceParam('Branch'){
+            description("Select the Branch to be built.")
+            filterable()
+            choiceType('SINGLE_SELECT')
+            groovyScript{
+                script('["master","develop"]')
+                fallbackScript('"fallback choice"')
+            }
+        }
+        definition{
+            cps() {
+                def jobScript = readFileFromWorkspace('cit.groovy')
+                script(pipelineJob)
+                def approvals = org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval.get()
+                approvals.approveScript(approvals.hash(jobScript,"groovy"))
+            }
+        }
+    }
+
+}
