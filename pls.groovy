b06bb6bd5f8f40f8143e1ddf205d521560340c3b
jayabalandevops <balaniaf@gmail.com>
2019-10-19 06:17:32 +0530

    modified pls.groovy

--- pls.groovy
+++ pls.groovy
@@ -15,14 +15,14 @@ pipelineJob("$BUName/$ProductName/CICD_$AppName"){
                 fallbackScript('"fallback choice"')
             }
         }
-        definition{
+    }
+    definition{
             cps {
                 def jobScript = readFileFromWorkspace('cit.groovy')
                 script(jobScript)
                 def approvals = org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval.get()
                 approvals.approveScript(approvals.hash(jobScript,"groovy"))
             }
-        }
     }
 
 }
